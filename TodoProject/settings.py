37d1d2ad953dc43aa9ef3772108f198ea6fce56d
omari joseph <Omarirobert98@gmail.com>
2026-08-18 01:39:24 -0700

    Add STATIC_ROOT configuration for static files

--- TodoProject/settings.py
+++ TodoProject/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 #STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
